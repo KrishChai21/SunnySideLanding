e4187c3ba8c9c8a0df0518c43c8a04a9fda9c662
KrishChai21 <85996682+KrishChai21@users.noreply.github.com>
2021-07-12 20:14:37 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+var menu = document.querySelector(".menu");
+var container = document.querySelector(".menu-container")
+
+menu.addEventListener("click",function(){
+    container.classList.toggle("show");
+})
+
